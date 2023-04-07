c6289d256c61e413dd989c9e5a938a04e12c69cf
이다온 <ondayary@gmail.com>
2023-04-08 00:14:03 +0900

    feat: 입력한 정보 컨트롤러로 전달하기 회원가입 페이지에서 입력한 정보를 RequestParam을 이용하여 전달온 정보를 받는다.

--- src/main/java/com/example/jpa/controller/MemberController.java
+++ src/main/java/com/example/jpa/controller/MemberController.java
@@ -2,6 +2,8 @@ package com.example.jpa.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MemberController {
@@ -9,5 +11,16 @@ public class MemberController {
     @GetMapping("/member/join")
     public String joinForm() {
         return "join";
+    }
+
+    // join 정보를 받아주는 메서드
+    @PostMapping("/member/join")
+    public String join(@RequestParam("memberEmail") String memberEmail,
+                       @RequestParam("memberPassword") String memberPassword,
+                       @RequestParam("memberName") String memberName) {
+        // RequestParam을 이용해서 ("")안에 name 값을 적어준다.
+        // name에 담은 값을 memberEmail에 옮겨담는다.
+        System.out.println("memberEmail = " + memberEmail + ", " + "memberPassword = " + memberPassword + ", memberName = " + memberName);
+        return "index";
     }
 }
