9eb6d7b1342bf1449d735853f2fc9ba8fd6632d5
이다온 <ondayary@gmail.com>
2023-04-21 09:39:28 +0900

    feat: 파일 첨부 요청에 필요한 필드 생성

--- src/main/java/com/example/jpa/dto/BoardDto.java
+++ src/main/java/com/example/jpa/dto/BoardDto.java
@@ -2,6 +2,7 @@ package com.example.jpa.dto;
 
 import com.example.jpa.entity.BoardEntity;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 
@@ -19,6 +20,12 @@ public class BoardDto { // DTO(Data Transfer Object)
     private int boardHits; // 조회수
     private LocalDateTime boardCreatedTime; // 작성시간
     private LocalDateTime boardUpdatedTime; // 수정시간
+
+    // 파일 첨부 관련
+    private MultipartFile boardFile; // multifile이라는 interface, write.html -> Contoller 파일 담는 용도
+    private String originalFileName; // 원본 파일 이름
+    private String storedFileName; // 서버 저장용 파일 이름
+    private int fileAttached; // 파일 첨부 여부(첨부 1, 미첨부 0)
 
     // 페이징 처리로 인해 페이지의 원하는 목록들만 보여주는 생성자 추가
     public BoardDto(Long id, String boardWriter, String boardTitle, int boardHits, LocalDateTime boardCreatedTime) {
